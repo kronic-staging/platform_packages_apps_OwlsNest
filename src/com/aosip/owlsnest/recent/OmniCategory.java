1d0253383c16870dab5a00bf474dca1922dbb754
xlxfoxxlx <joshfox87@gmail.com>
2016-09-12 22:37:46 -0400

    Change up omniswitch settings
    Change-Id: I041f03daf2224855e5a62b10df91807921ae1b13

--- src/com/aosip/owlsnest/recent/OmniCategory.java
+++ src/com/aosip/owlsnest/recent/OmniCategory.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.recent;
+package com.android.settings.aosip.recent;
 
 import android.app.ActivityManager;
 import android.content.Context;
@@ -39,7 +39,7 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class RecentCategory extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class OmniCategory extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_OMNISWITCH = "omniswitch";
     private static final String KEY_OMNI_SWITCH_PACKAGE_NAME = "org.omnirom.omniswitch";
@@ -63,7 +63,7 @@ public class RecentCategory extends SettingsPreferenceFragment implements OnPref
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.aosip_recent);
+        addPreferencesFromResource(R.xml.aosip_omni);
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
 
