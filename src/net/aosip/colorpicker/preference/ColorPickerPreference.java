ResurrectionRemix/Resurrection_packages_apps_Settings

Color picker enable alpha slider as default

src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -52,7 +52,7 @@ public class ColorPickerPreference extends DialogPreference implements
     int mDarkKatColor = mAndroidColor;
     private int mValue = Color.BLACK;
     private float mDensity = 0;
-    private boolean mAlphaSliderEnabled = false;
+    private boolean mAlphaSliderEnabled = true;
 
     private static final String androidns = "http://schemas.android.com/apk/res/android";
 

kronic-staging/platform_packages_apps_OwlsNest

Change-Id: I86b3d2f081398b4475c17faabae5e4bee05e66ef

src/net/aosip/colorpicker/preference/ColorPickerPreference.java
@@ -66,7 +66,7 @@ public class ColorPickerPreference extends Preference implements
     private String mResetColor1Title = null;
     private String mResetColor2Title = null;
     private int mValue;
-    private boolean mAlphaSliderVisible = false;
+    private boolean mAlphaSliderVisible = true;
 
     public ColorPickerPreference(Context context) {
         this(context, null);